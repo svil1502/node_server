b45dd64c2f31beb2a493e25d5d18b3aeea2e389a
Svelana Ilina <developersvil@gmail.com>
2020-10-02 17:10:11 +0300

    Первый коммит

---
+++ public/js/checkout.js
@@ -0,0 +1,11 @@
+function selectone(item) {
+    document.getElementById("firstLevelname").innerHTML = item;
+}
+
+function selecttwo(item) {
+    document.getElementById("secondLevelname").innerHTML = item;
+}
+
+function addLevelOne(){
+    alert("Click");
+}
---
+++ public/js/line.dir.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class LineDir {
+    constructor() {
+        this.path = "";
+        this.name = "";
+        this.img = "";
+        this.isDir = false;
+        this.array = [];
+        this.ext = "";
+        this.price = "";
+    }
+}
+
+module.exports = LineDir;
